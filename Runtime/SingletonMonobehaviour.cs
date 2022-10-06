33b56faa533385d92b8323931863d1d7898732d2
msavioti <angelosavioti@gmail.com>
2022-10-05 23:12:23 -0300

    refactor: moved class into namespace

--- Runtime/SingletonMonobehaviour.cs
+++ Runtime/SingletonMonobehaviour.cs
@@ -3,134 +3,137 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SingletonMonobehaviour<T> : MonoBehaviour where T : Component
+namespace SoftBoiledGames.Singleton
 {
-    #region Static fields
-
-    // Singleton _instance.
-    private static T _instance;
-    
-    #endregion
-
-    #region Serialized fields
-
-    [Header("Singleton settings")]
-    [SerializeField]
-    private bool _createObjectIfNotInScene = false;
-
-    [SerializeField]
-    private bool _persistBetweenScenes = false;
-
-    #endregion
-
-    #region Properties
-
-    public static T Instance
+    public class SingletonMonobehaviour<T> : MonoBehaviour where T : Component
     {
-        get
+        #region Static fields
+
+        // Singleton _instance.
+        private static T _instance;
+
+        #endregion
+
+        #region Serialized fields
+
+        [Header("Singleton settings")]
+        [SerializeField]
+        private bool _createObjectIfNotInScene = false;
+
+        [SerializeField]
+        private bool _persistBetweenScenes = false;
+
+        #endregion
+
+        #region Properties
+
+        public static T Instance
         {
-            if (_isQuitting)
+            get
             {
-                return null;
+                if (_isQuitting)
+                {
+                    return null;
+                }
+
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Non-serialized fields
+
+        private static bool _isQuitting = false;
+        #endregion
+
+        #region Unity events
+
+        // Remember overriding and calling base.Awake() in
+        // classes which extend this one.
+        protected virtual void Awake() 
+        {
+            VerifySingletonPattern();
+            CreateSingletonObject();
+        }
+
+        protected virtual void OnApplicationQuit() 
+        {
+            _isQuitting = true;
+        }
+
+        #endregion
+
+        #region Public methods
+        #endregion
+
+        #region Private methods
+
+        private void VerifySingletonPattern()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
             }
 
-            return _instance;
-        }
-    }
+            if (_instance != null)
+            {
+                string warning = "An additional singleton _instance was created";
+                warning += $" in {gameObject.name}. Destroying it now...";
+                Debug.LogWarning(warning);
+                Destroy(gameObject);
+            }
+            else
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                }
 
-    #endregion
+                if (_instance == null)
+                {
+                    _instance = this as T;
+                }
 
-    #region Non-serialized fields
-
-    private static bool _isQuitting = false;
-    #endregion
-
-    #region Unity events
-
-    // Remember overriding and calling base.Awake() in
-    // classes which extend this one.
-    protected virtual void Awake() 
-    {
-        VerifySingletonPattern();
-        CreateSingletonObject();
-    }
-
-    protected virtual void OnApplicationQuit() 
-    {
-        _isQuitting = true;
-    }
-
-    #endregion
-
-    #region Public methods
-    #endregion
-
-    #region Private methods
-
-    private void VerifySingletonPattern()
-    {
-        if (!Application.isPlaying)
-        {
-            return;
+                if (_instance == null)
+                {
+                    Debug.LogWarning("Singleton object not found.");
+                }
+            }
         }
 
-        if (_instance != null)
+        private void CreateSingletonObject()
         {
-            string warning = "An additional singleton _instance was created";
-            warning += $" in {gameObject.name}. Destroying it now...";
-            Debug.LogWarning(warning);
-            Destroy(gameObject);
+            if (!_createObjectIfNotInScene)
+            {
+                return;
+            }
+
+            if (_instance != null)
+            {
+                return;
+            }
+
+            GameObject singletonObject = new GameObject();
+            _instance = singletonObject.AddComponent<T>();
+            Debug.Log("Singleton object created.");
         }
-        else
+
+        private void MakeObjectPersistent()
         {
+            if (!_persistBetweenScenes)
+            {
+                return;
+            }
+
             if (_instance == null)
             {
-                _instance = FindObjectOfType<T>();
+                return;
             }
 
-            if (_instance == null)
-            {
-                _instance = this as T;
-            }
-
-            if (_instance == null)
-            {
-                Debug.LogWarning("Singleton object not found.");
-            }
+            DontDestroyOnLoad(gameObject);
         }
+
+        #endregion    
     }
-
-    private void CreateSingletonObject()
-    {
-        if (!_createObjectIfNotInScene)
-        {
-            return;
-        }
-
-        if (_instance != null)
-        {
-            return;
-        }
-
-        GameObject singletonObject = new GameObject();
-        _instance = singletonObject.AddComponent<T>();
-        Debug.Log("Singleton object created.");
-    }
-
-    private void MakeObjectPersistent()
-    {
-        if (!_persistBetweenScenes)
-        {
-            return;
-        }
-
-        if (_instance == null)
-        {
-            return;
-        }
-
-        DontDestroyOnLoad(gameObject);
-    }
-
-    #endregion    
 }
